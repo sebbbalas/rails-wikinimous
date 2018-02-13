fbedf750fa6aebe53d992fdd11d3863646dd5e70
Sebastien Balas <balas.sebastien@gmail.com>
2018-02-13 16:59:30 +0100

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+# TODO: Write a seed to insert 100 posts in the database
+puts 'Creating 10 fake posts...'
+10.times do
+  article = Article.new(
+    title: Faker::ChuckNorris.fact,
+    content: Faker::HarryPotter.quote
+  )
+  article.save!
+end
+puts 'Finished!'
+# end
